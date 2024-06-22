a4a1196cb1212dd4a113136d11ca9343cbcdc117
0chuhh <vvlasovdo@mail.ru>
2024-06-23 02:56:11 +0900

    feat: setup reducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,25 @@
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { entityAPI } from "services/entity-service";
+import { rowAPI } from "services/row-service";
+import entity from "./reducers/entity/entity-slice";
 
 const rootReducer = combineReducers({
-})
+    entity,
+    [entityAPI.reducerPath]: entityAPI.reducer,
+    [rowAPI.reducerPath]: rowAPI.reducer
+});
 
 export const setupStore = () => {
     return configureStore({
-        reducer: rootReducer
-    })
-}
+        reducer: rootReducer,
+        middleware: (getDefultMiddleware) =>
+            getDefultMiddleware().concat(
+                entityAPI.middleware,
+                rowAPI.middleware
+            )
+    });
+};
 
-export type RootState = ReturnType<typeof rootReducer>
-export type AppStore = ReturnType<typeof setupStore>
-export type AppDispatch = AppStore['dispatch']
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
